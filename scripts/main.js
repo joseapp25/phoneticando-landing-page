b27c9c3d472a0ef995eb7a9530fb3230fa088250
joseapp25 <joseapp25@gmail.com>
2026-06-10 15:48:20 -0300

    ipa added

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const letters = document.querySelectorAll(".alphabet-letter, .voiced, .voiceless");
+
+    letters.forEach(letter => {
+
+        letter.addEventListener("mouseenter", () => {
+        if (!letter.classList.contains("clicked-alphabet") &&
+        !letter.classList.contains("clicked-voiced") &&
+        !letter.classList.contains("clicked-voiceless")) {
+
+        if (letter.classList.contains("alphabet-letter")) {
+        letter.classList.add("hovered-alphabet");
+        } else if (letter.classList.contains("voiced")) {
+        letter.classList.add("hovered-voiced");
+        } else if (letter.classList.contains("voiceless")) {
+        letter.classList.add("hovered-voiceless");
+        }
+        }
+        });
+
+        letter.addEventListener("mouseleave", () => {
+        letter.classList.remove("hovered-alphabet", "hovered-voiced", "hovered-voiceless");
+        });
+
+        letter.addEventListener("click", () => {
+        const audioSrc = letter.getAttribute("data-audio");
+        const audio = new Audio(audioSrc);
+
+        letter.classList.remove("hovered-alphabet", "hovered-voiced", "hovered-voiceless");
+
+        if (letter.classList.contains("alphabet-letter")) {
+        letter.classList.add("clicked-alphabet");
+        } else if (letter.classList.contains("voiced")) {
+        letter.classList.add("clicked-voiced");
+        } else if (letter.classList.contains("voiceless")) {
+        letter.classList.add("clicked-voiceless");
+        }
+
+        audio.addEventListener("ended", () => {
+        letter.classList.remove("clicked-alphabet", "clicked-voiced", "clicked-voiceless");
+        });
+
+        audio.play();
+        });
+
+    });
+
+});
