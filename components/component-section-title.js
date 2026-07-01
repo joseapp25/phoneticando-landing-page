92fe773ccf855480a7e96899941fccf969c3e79d
joseapp25 <joseapp25@gmail.com>
2026-07-01 07:30:15 -0300

    font size

--- components/component-section-title.js
+++ components/component-section-title.js
@@ -25,7 +25,7 @@ class ComponentSectionTitle extends HTMLElement{
                 .highlight-text {
                     max-width: 600px;
                     display: flex;
-                    font-size: 80px;
+                    font-size: 64px;
                     font-weight: 600;
                     font-family: var(--font-inter-tight);
                     text-shadow: 2px 2px 0px var(--graycool1000);
@@ -33,7 +33,7 @@ class ComponentSectionTitle extends HTMLElement{
 
                 @media only screen and (min-width: 600px) {
                     .highlight-text {
-                        font-size: 96px;
+                        font-size: 80px;
                     }
                 }
 
