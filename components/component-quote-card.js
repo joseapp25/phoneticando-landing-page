6c95a7ae4b791863e19543739abaa75e13dc2c1f
joseapp25 <joseapp25@gmail.com>
2026-07-17 16:45:54 -0300

    quotation added

---
+++ components/component-quote-card.js
@@ -0,0 +1,62 @@
+class ComponentQuoteCard extends HTMLElement{
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+
+            <link rel="stylesheet" href="styles/style.css">
+
+            <style>
+
+            .quotation-container {
+                background-color: var(--graycool0);
+                color: var(--graycool1000);
+                border-left: 8px solid var(--sunset600);
+                padding: 16px;
+                margin-bottom: 24px;
+                margin-top: 24px;
+                border-radius: 4px;
+            }
+
+            .quote-container {
+                margin-bottom: 8px;
+            }
+
+            .quote-style {
+                font-weight: 700;
+                font-size: 1.2rem;
+            }
+
+            .author-container {
+                margin-top: 8px;
+            }
+
+            .author-style {
+                color: var(--cerulean600);
+                font-style: italic;
+                font-weight: 600;
+            }
+
+
+            </style>
+
+            <div class="quotation-container">
+                <div class="quote-container">
+                    <p>
+                        <q class="quote-style">
+                            <slot name="quote"></slot>
+                        </q>
+                    </p>
+                </div>
+                <div class="author-container">
+                    <p class="author-style">
+                        <slot name="author"></slot>
+                    </p>
+                </div>
+            </div>
+
+        `
+    }
+}
+
+customElements.define('component-quote-card', ComponentQuoteCard);
